2c56a01356e70cbf5e5b442664c238173f1d5fdf
krypton97 <bldcuber@gmail.com>
2017-05-21 20:58:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -20,8 +20,5 @@ io.on('connection', socket => {
   });
 });
 
-
-server.listen(process.env.PORT,  () => {
-    console.log(`Server is running on port 3000`)
-})
+server.listen(process.env.PORT,  () => console.log(`Server is running on port 3000`))
 
